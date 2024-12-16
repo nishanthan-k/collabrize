e06c56d04d0988c8c558cdc772890378f2ddc642
nishanthan-k <krn.nishanthan@gmail.com>
2024-12-16 16:13:40 +0530

    fix: cors frontend domain

--- backend/index.ts
+++ backend/index.ts
@@ -15,6 +15,8 @@ const port = config.app.port;
 const corsOptions = {
   origin: [domain, frontendDevDomain,  frontendProdDomain],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
