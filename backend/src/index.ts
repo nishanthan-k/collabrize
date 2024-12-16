fb49d7bc8eae1d7e3dfb39b40a1cb5ec86cc7f03
nishanthan-k <krn.nishanthan@gmail.com>
2024-12-16 13:22:09 +0530

    chore: check server running with request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,6 +20,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Server is running no issues");
+});
+
 app.use('/api/auth', authRouter);
 
 // middlewares
